136e4680c0adcae974ffed8e9b697095fd6a53a2
LukaKmetec <luka.kmetec@tscng.org>
2025-02-03 07:54:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -86,3 +86,8 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+function resetiraj(){
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
