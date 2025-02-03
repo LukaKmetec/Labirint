21f9ef11f6cbcabe01532c0cc2a900dfe5b32382
LukaKmetec <luka.kmetec@tscng.org>
2025-02-03 08:16:43 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -90,4 +90,4 @@ function resetiraj(){
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
+};
